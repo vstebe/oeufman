dc8bb1d7ef4a319b406011aa8de50b0bfcb69104
nico <nico.mandica@orange.fr>
2016-01-28 12:13:59 +0100

    Ajout fonctions noeuds

--- oeufman/node.cpp
+++ oeufman/node.cpp
@@ -4,8 +4,73 @@ Node::Node():
     value(0),
     frequency(0),
     left(0),
-    right(0)
+    right(0),
+    isLeaf(false)
 {
 
+}
 
+Node::Node(Node *left, Node *right):
+    value(0),
+    frequency(0),
+    isLeaf(false)
+{
+    this->left = left;
+    this->right = right;
+}
+
+Node::Node(unsigned char value, unsigned int frequency):
+    value(value),
+    frequency(frequency),
+    isLeaf(true)
+{
+
+}
+
+unsigned int Node::count()
+{
+    int frequency = 0;
+
+    if(left)
+        frequency += left->count();
+    if(right)
+        frequency += right->count();
+
+    frequency += this->frequency;
+
+    return frequency;
+}
+
+void Node::updateFrequencies()
+{
+    frequency = count();
+    if(left)
+        left->updateFrequencies();
+    if(right)
+        right->updateFrequencies();
+}
+
+bool Node::getIsLeaf() const
+{
+    return isLeaf;
+}
+
+unsigned char Node::getValue() const
+{
+    return value;
+}
+
+void Node::setValue(unsigned char value)
+{
+    this->value = value;
+}
+
+unsigned int Node::getFrequency() const
+{
+    return frequency;
+}
+
+void Node::setFrequency(unsigned int value)
+{
+    frequency = value;
 }
--- oeufman/node.h
+++ oeufman/node.h
@@ -6,10 +6,25 @@ class Node
 {
 public:
     Node();
+    Node(Node *left, Node *right);
+    Node(unsigned char value, unsigned int frequency);
+    unsigned int count();
+    void updateFrequencies();
+
+    bool getIsLeaf() const;
+
+    unsigned char getValue() const;
+    void setValue(unsigned char value);
+
+    unsigned int getFrequency() const;
+    void setFrequency(unsigned int value);
+
+protected:
     unsigned char value;
     unsigned int frequency;
     Node *left;
     Node *right;
+    bool isLeaf;
 };
 
 #endif // NODE_H
